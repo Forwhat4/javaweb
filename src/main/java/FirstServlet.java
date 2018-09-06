822d13906929773eeb707e7763eb34b7549e9cc6
Forwhat4 <15057763977@163.com>
2018-09-06 17:29:25 +0800

    Initial commit

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+
+@WebServlet("/hello")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("currentDate",new Date());
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: WYY
+  Date: 2018/9/6
+  Time: 16:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+
+    <span>当前时间：${currentDate}</span>
+
+</body>
+</html>
